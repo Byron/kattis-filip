505a01a14cdd8db3040a4d72855012834c08ee7d
Sebastian Thiel <byronimo@gmail.com>
2018-11-11 14:57:12 +0100

    solution

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use std::{io, io::Read};
+use std::{io, io::Read, str::FromStr};
 
 fn main() -> Result<(), Box<std::error::Error + 'static>> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
+    let input: String = input.trim().chars().rev().collect();
+    let mut nums = input.split(' ').map(usize::from_str).filter_map(Result::ok);
+    match (nums.next(), nums.next()) {
+        (Some(first), Some(second)) => println!("{}", if first > second { first } else { second }),
+        _ => panic!("exhausted"),
+    };
     Ok(())
 }
